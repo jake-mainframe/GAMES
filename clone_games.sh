781ecc1b9a27eddf1c373ce9bd389ad269ca071c
Jake <jake.labelle@hotmail.co.uk>
2021-06-19 14:07:44 +0100

    games metapackage

---
+++ clone_games.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ../SOFTWARE
+
+git clone https://github.com/MVS-sysgen/Adventure ADVENT
+git clone https://github.com/jake-mainframe/SNAKE
+git clone https://github.com/jake-mainframe/WORM
