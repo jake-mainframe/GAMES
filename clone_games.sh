c3b5eb06d599ec4aaa81535e91fe8b686e542ad4
Jake <jake.labelle@hotmail.co.uk>
2021-06-19 15:13:16 +0100

    adding cbt568

--- clone_games.sh
+++ clone_games.sh
@@ -5,3 +5,4 @@ cd ../SOFTWARE
 git clone https://github.com/MVS-sysgen/Adventure ADVENT
 git clone https://github.com/jake-mainframe/SNAKE
 git clone https://github.com/jake-mainframe/WORM
+git clone https://github.com/jake-mainframe/CBT568
